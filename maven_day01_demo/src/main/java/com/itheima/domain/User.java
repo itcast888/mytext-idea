05ea6598e375cb0ae43fccbb54440b0bf332b9ea
itcast <bos@itcast.cn>
2020-03-24 11:15:07 +0800

    123

---
+++ maven_day01_demo/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author ly
+ * @Company 深圳黑马程序员
+ * @Date 2020/3/24 11:12
+ * @Version V1.0
+ */
+public class User {
+}
